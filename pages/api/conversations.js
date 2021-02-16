138f4672bae4b9acbdbc0d53ded39fea6f5a2a06
luizhenrik <luhemeso@gmail.com>
2021-02-16 14:32:41 -0300

    corrigindo api conversations

--- pages/api/conversations.js
+++ pages/api/conversations.js
@@ -1,4 +1,4 @@
-async function users(request, response) {
+async function conversations(request, response) {
   const resChat = await fetch('https://xirrim.com/api/chats')
   const chat = await resChat.json()
 
@@ -7,23 +7,22 @@ async function users(request, response) {
       name: 'Lorem Ipsum',
       userId: 'oxAhz',
       chatId: 'q2e3r6Zq',
-      messages: ''
+      messages: []
     },
     {
       name: 'Lorem Ipsum 2',
       userId: 'mnhzAS',
       chatId: 'q1e2r4Zq',
-      messages: ''
+      messages: []
     }
   ]
 
   data.map((value, index) => {
     const arr = chat[data[index].chatId].messages.length
-
-    data.messages = chat[data[index].chatId].messages[arr - 1]
+    data[index].messages = chat[data[index].chatId].messages[arr - 1]
   })
 
   response.status(200).json(data)
 }
 
-export default users
+export default conversations
