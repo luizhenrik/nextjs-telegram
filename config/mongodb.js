9e83b22e618870f9b7fcce2ef3c3353497b821cd
luizhenrik <luhemeso@gmail.com>
2021-02-19 21:03:07 -0300

    vercel cli

--- config/mongodb.js
+++ config/mongodb.js
@@ -9,7 +9,7 @@ const client = new MongoClient(`${process.env.MONGODB_URI}`, {
 async function database(req, res, next) {
   if (!client.isConnected()) await client.connect()
   req.dbClient = client
-  req.db = client.db(`${process.env.MONGODB_DB}`)
+  req.db = client.db('telegram')
   return next()
 }
 
