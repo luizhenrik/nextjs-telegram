aa98980fc533438d319c8ec1fa2950db07e7a7b6
luizhenrik <luhemeso@gmail.com>
2021-03-06 22:45:46 -0300

    fix prefetch

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ function Home() {
                 <Header></Header>
                 <Sidebar></Sidebar>
                 <div className={'app__container'}>
-                    <Link prefetch={false} as={'/chats'} href={'/chats'}>
+                    <Link as={'/chats'} href={'/chats'}>
                         <a className={'app__title'}>
                             Clique aqui para conectar no app
                         </a>
